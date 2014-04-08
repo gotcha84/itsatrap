0992a8d3ec05bdd21841dc9344d31ab722894e16
Michael Yao <mify21@gmail.com>
2014-04-08 08:36:39 -0700

    update
    code guard for header file

--- network/packet.h
+++ network/packet.h
@@ -1,10 +1,17 @@
+#ifndef PACKET_H
+#define PACKET_H
 
+// Packet for clients
 struct packet {
 	int eventId;	
 };
 
+
+// Initial connection between client/server
 struct initResponsePacket
 {
 	int eventId;
 	int givenPlayerId;
 };
+
+#endif
