ebcd04d9e42c34335a98e70505c09e7fab6694b6
unknown <akso@ACS-CSEB220-17.ad.ucsd.edu>
2014-06-06 06:35:07 -0700

    tried lighting

--- itsatrap/ItsATrapClient/main.cpp
+++ itsatrap/ItsATrapClient/main.cpp
@@ -88,12 +88,45 @@ int main(int argc, char *argv[]) {
 	ConfigSettings::getConfig()->loadSettingsFile();
 
 	// opengl lighting
-	float specular[]  = {1.0, 1.0, 1.0, 1.0};
-	float shininess[] = {100.0};
-	//float position[]  = {0.0, 10.0, 1.0, 0.0};  // lightsource position
-	GLfloat position[]  = {0.0f, 500.0f, -250.0f, 1.0f};  // lightsource position
-	GLfloat ambientLight[] = {0.7f, 0.7f, 0.7f, 1.0f};
-	GLfloat diffuseLight[] = {0.5f, 0.5f, 0.5f, 1.0f};
+	
+	GLfloat shininess[] = {100.0};
+	GLfloat spotDirection[] = { 0.0f, -1.0f, 0.0f };
+
+	GLfloat specularLight0[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	GLfloat position0[] = { 0.0f, -1200.0f, -300.0f, 0.0f };  // lightsource position
+	GLfloat ambientLight0[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	GLfloat diffuseLight0[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	
+	GLfloat specularLight1[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat position1[]  = {-20.0f, -100.0f, -350.0f, 1.0f};  // lightsource position
+	GLfloat ambientLight1[] = { 0.4f, 0.8f, 0.8f, 1.0f };
+	GLfloat diffuseLight1[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+	GLfloat specularLight2[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	GLfloat position2[] = { 0.0f, -1200.0f, 300.0f, 0.0f };  // lightsource position
+	GLfloat ambientLight2[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	GLfloat diffuseLight2[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+
+	GLfloat specularLight3[] = { 1.0f, 0.2f, 0.2f, 1.0f };
+	GLfloat position3[] = { -350.0f, 1000.0f, -350.0f, 1.0f };  // lightsource position
+	GLfloat ambientLight3[] = { 0.6f, 0.2f, 0.2f, 1.0f };
+	GLfloat diffuseLight3[] = { 1.0f, 0.2f, 0.2f, 1.0f };
+
+	GLfloat specularLight4[] = { 0.2f, 1.0f, 0.2f, 1.0f };
+	GLfloat position4[] = { 350.0f, 1000.0f, -350.0f, 1.0f };  // lightsource position
+	GLfloat ambientLight4[] = { 0.2f, 0.6f, 0.2f, 1.0f };
+	GLfloat diffuseLight4[] = { 0.2f, 1.0f, 0.2f, 1.0f };
+
+	GLfloat specularLight5[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	GLfloat position5[] = { -300.0f, -1200.0f, 0.0f, 0.0f };  // lightsource position
+	GLfloat ambientLight5[] = { 0.4f, 0.4f, 0.8f, 1.0f };
+	GLfloat diffuseLight5[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+
+	GLfloat specularLight6[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+	GLfloat position6[] = { -300.0f, -1200.0f, 0.0f, 0.0f };  // lightsource position
+	GLfloat ambientLight6[] = { 0.4f, 0.4f, 0.8f, 1.0f };
+	GLfloat diffuseLight6[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+
 
 	// init glut
 	glutInit(&argc, argv);                      // initialize GLUT
@@ -158,18 +191,41 @@ int main(int argc, char *argv[]) {
 	
 	// Generate material properties:
 	//glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
-	//glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
 	//glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 	glEnable(GL_COLOR_MATERIAL);
 	
 	// Generate light source:
 	//glDisable(GL_LIGHTING);
 	//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
-	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight0);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight0);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight0);
+	glLightfv(GL_LIGHT0, GL_POSITION, position0);
+	
+	/*
+	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 45.0);
+	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spotDirection);
+	glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 2.0);
+	*/
+	/*
+	glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1.5);
+	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.5);
+	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.2);
+	*/
+
+	glLightfv(GL_LIGHT2, GL_SPECULAR, specularLight2);
+	glLightfv(GL_LIGHT2, GL_AMBIENT, ambientLight2);
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, diffuseLight2);
+	glLightfv(GL_LIGHT2, GL_POSITION, position2);
+
+	glLightf(GL_LIGHT2, GL_CONSTANT_ATTENUATION, 1.5);
+	glLightf(GL_LIGHT2, GL_LINEAR_ATTENUATION, 0.5);
+	glLightf(GL_LIGHT2, GL_QUADRATIC_ATTENUATION, 0.2);
+	
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT2);
 	
 	// Install callback functions:
 	glutDisplayFunc(window->displayCallback);
@@ -196,7 +252,7 @@ int main(int argc, char *argv[]) {
 	glutPassiveMotionFunc(window->processMouseMove);
 
 	// hide mouse cursor
-	glutSetCursor(GLUT_CURSOR_NONE);
+	//glutSetCursor(GLUT_CURSOR_NONE);
 
 	// Sending AABBs to server
 	client->root->addChild(client->level.getRoot());
